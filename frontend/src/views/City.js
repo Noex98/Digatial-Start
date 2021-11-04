1ca473cd6d43db833678f9da9e4adfdaeb2c1eca
Noex98 <johannes.knickering@hotmail.com>
2021-11-04 04:47:15 +0100

    css og markup

--- frontend/src/views/City.js
+++ frontend/src/views/City.js
@@ -35,47 +35,53 @@ export default function City(){
 
     let inspiration_showed = []
 
-    function returnInspiration() {
-        let n = Math.floor(Math.random() * (dataset.length - 1) + 1)
+    function returnInspiration(n_repeat) {
 
-        // Avoid duplicates
-        while (inspiration_showed.includes(n)){
-            n = Math.floor(Math.random() * (dataset.length - 1) + 1)
-            console.log(1)
+        let output = ""
+
+        for (let i = 0; i < n_repeat; i++){
+            
+            let n = Math.floor(Math.random() * (dataset.length - 1) + 1)
+
+            // Avoid duplicate || make sure an image file is present
+            while (inspiration_showed.includes(n) || !dataset[n].Files[0]){
+                n = Math.floor(Math.random() * (dataset.length - 1) + 1)
+            }
+
+            inspiration_showed.push(n)
+
+            let item = dataset[n]
+
+            output += (/*html*/`
+                <div class="inspiration__item">
+                    <div class="item__imgWrap">
+                        <img src="${item.Files[0].Uri}" alt="${item.Files[0].AltText}"/>
+                        <div class="imgWrap__text">
+                            ${item.Name}
+                        </div>
+                    </div>
+                    <div class="item__text">
+                        ${item.Descriptions[item.Descriptions.length - 1].Text}
+                    </div>
+                    <div class="item__cta">
+                        SE MERE <i class="fas fa-chevron-right"></i>
+                    </div>
+
+                </div>
+            `)
         }
 
-        inspiration_showed.push(n)
-
-        let item = dataset[n]
-
-        console.log(item)
-
-        return (/*html*/`
-            <div class="inspiration__item">
-                <div class="item__imgWrap">
-                    ${item.Files ? `<img src="${item.Files[0].Uri}" alt="${item.Files[0].AltText}"/>` : ""}
-                    <div class="imgWrap__text">
-                        ${item.Name}
-                    </div>
-                </div>
-
-            </div>
-        `)
-
+        return output
         
     }
 
     window.showMore = () => {
-        let output = ""
-        for (let i = 0; i < 3; i++){
-            output += returnInspiration()
-        }
-        return output
+        document.querySelector('.city__inspiration').innerHTML += returnInspiration(3)
     }
 
     return (/*html*/`
+        ${Header({backBtn: true, destination: '/'})}
         <div class="root__city">
-            ${Header({backBtn: true, destination: '/'})}
             <div class="city__introImg">
                 <img src="${city.img.url}" alt="${city.img.alt}" />
                 <h1>${city.name.da}</h1>
@@ -102,8 +108,9 @@ export default function City(){
                 </div>
             </div>
             <div class="city__inspiration">
-                ${showMore()}
+                ${returnInspiration(4)}
             </div>
+            <div class="city__showMore" onclick="showMore()">Se flere</div>
         </div>
     `)   
 
